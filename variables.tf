410588efc2152eb90d042cfa5f266cce8368ab11
mitraman <ronnie.mitra@gmail.com>
2020-07-26 15:04:02 +0100

    debugging

--- variables.tf
+++ variables.tf
@@ -1,16 +1,34 @@
 variable "aws_region" {
-  type    = string
-  default = "eu-west-2"
+  type = string
 }
 
 variable "tf_group_name" {
   type = string
 }
 
-variable "vpc_id" {
+variable "subnet_a_id" {
+  type = string
+}
+
+variable "subnet_b_id" {
   type = string
 }
 
 variable "env_name" {
   type = string
+}
+
+variable "mysql_user" {
+  type    = string
+  default = "microservices"
+}
+
+variable "mysql_password" {
+  type    = string
+  default = "microservices"
+}
+
+variable "mysql_database" {
+  type    = string
+  default = "microservices_db"
 }
